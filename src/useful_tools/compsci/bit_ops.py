96739261f6aa7bacac88ede06d02ce15c590bd45
Stickie <71910589+FieryIceStickie@users.noreply.github.com>
2023-02-05 18:17:41 +1100

    Merge bit ops into project (#2)
    * Added binary operation stuff
    
    * Clean up of bit_ops.py
    
    * Clean up of bit_ops.py
    
    * Clean up of bit_ops.py

---
+++ src/useful_tools/compsci/bit_ops.py
@@ -0,0 +1,24 @@
+from einspect import impl
+
+
+@impl(int)
+def __matmul__(self, other):
+    return int(not(self&other))
+
+
+def full_adder(a: int, b: int, c: int):
+    return a@b@(a@b@a@(a@b@b)@c), a@b@a@(a@b@b)@c@(a@b@a@(a@b@b))@(a@b@a@(a@b@b)@c@c)
+
+
+def add(a, b):
+    c, *n = 0,
+    while a | b:
+        c, i = full_adder(a & 1, b & 1, c)
+        n += [i]
+        a >>= 1
+        b >>= 1
+    return [c, *n[::-1]]
+
+
+if __name__ == '__main__':
+    pass
