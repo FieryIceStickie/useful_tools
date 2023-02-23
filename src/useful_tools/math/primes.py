d6b18a999b66bedba3a216afdea5100c8236d188
Stickie <usethecorrectformat@gmail.com>
2023-02-23 11:08:23 +1100

    - Fixed polynomial.py imports - Added partition function to primes.py

--- src/useful_tools/math/primes.py
+++ src/useful_tools/math/primes.py
@@ -2,6 +2,9 @@ import math
 from collections import Counter
 from functools import lru_cache
 from itertools import chain, product
+from typing import Iterator, overload, Literal
+from bisect import bisect
+from operator import itemgetter
 
 from src.useful_tools.utils import round_robin
 
@@ -93,9 +96,13 @@ def egyptian_decomposition(p: int, q: int, /, *,
         if not (q+1) % p:
             b = (q+1) // p
             return [b, b*q]
+
         # Interpolation search factors of q+1 that sum to p
+
         # Practical number A: 2/p = 1/A + (2A-p)/Ap
+
         # Check if q is practical
+
         # 2/q = 1/q + 1/2q + 1/3q + 1/6q factorization for q prime
     denoms = []
     while True:
@@ -107,6 +114,47 @@ def egyptian_decomposition(p: int, q: int, /, *,
         q //= g
         if p == 1:
             return denoms + [q]
+
+
+@overload
+def generalized_pentagonal_nums(n: int, *, include_index: Literal[True] = False) -> Iterator[tuple[int, int]]:
+    ...
+
+
+@overload
+def generalized_pentagonal_nums(n: int, *, include_index: Literal[False] = False) -> Iterator[int]:
+    ...
+
+
+def generalized_pentagonal_nums(n: int, *, include_index: bool = False) -> Iterator[int | tuple[int, int]]:
+    """
+    Generates the first n generalized pentagonal numbers, starting from 0
+    Also returns the number
+    https://en.wikipedia.org/wiki/Pentagonal_number
+    :param n: num
+    :param include_index: kwarg for including index in return type
+    :return: (k, kth generalized pentagonal number) if include_index else kth generalized pentagonal number
+    """
+    k = 0
+    for _ in range(n):
+        yield (k, int(k * (3*k - 1) // 2)) if include_index else int(k * (3*k - 1) // 2)
+        k = (k <= 0) - k
+
+
+def count_non_strict_integer_partitions(num: int) -> int:
+    """
+    https://en.wikipedia.org/wiki/Partition_(number_theory)
+    :param num: number
+    :return: number of partitions less than num
+    """
+    *penta_nums, = generalized_pentagonal_nums(num + 1, include_index=True)
+
+    @lru_cache(maxsize=num)
+    def p(n: int) -> int:
+        if n == 0:
+            return 1
+        return sum(p(n - a) * (1 if k % 2 else -1) for k, a in penta_nums[1:bisect(penta_nums, n, key=itemgetter(1))])
+    return p(num)
 
 
 if __name__ == '__main__':
